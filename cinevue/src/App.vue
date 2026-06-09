c6e41eb1d3ff736993b810f02bc1d4a336351f5b
Thiago <engs-thiagoluz@camporeal.edu.br>
2026-06-09 16:19:10 +0000

    Jun 9, 2026, 1:19 PM

--- cinevue/src/App.vue
+++ cinevue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="navbar">
     <div class="nav-container">
-      <h1 class="logo">CineVue - Filmes para todes e todes!</h1>
+      <h1 class="logo">CineVue - Filmes para todos e todes!</h1>
       <nav>
         <router-link to="/">Início</router-link>
         <router-link to="/contato">Feedback</router-link>
@@ -16,7 +16,7 @@
 
 <style scoped>
 .navbar {
-  background-color: rgb(208, 111, 247);
+  background-image: linear-gradient(red, blue);
   backdrop-filter: blur(10px);
   position: sticky;
   top: 0;
